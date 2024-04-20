523f0ef582022c43367fb4b7ca47666236c044cb
dmikazansky <dmi.kazansky@gmail.com>
2024-04-21 00:29:21 +0300

    Added logging using slf4j library

--- src/main/java/Horse.java
+++ src/main/java/Horse.java
@@ -1,3 +1,4 @@
+import org.slf4j.*;
 import static java.util.Objects.isNull;
 
 public class Horse {
@@ -5,23 +6,30 @@ public class Horse {
     private final String name;
     private final double speed;
     private double distance;
+    private static final Logger log = LoggerFactory.getLogger(Horse.class);
+
 
     public Horse(String name, double speed, double distance) {
         if (isNull(name)) {
+            log.error("Name is null");
             throw new IllegalArgumentException("Name cannot be null.");
         } else if (name.isBlank()) {
+            log.error("Name is blank");
             throw new IllegalArgumentException("Name cannot be blank.");
         }
         if (speed < 0) {
+            log.error("Speed is negative");
             throw new IllegalArgumentException("Speed cannot be negative.");
         }
         if (distance < 0) {
+            log.error("Distance is negative");
             throw new IllegalArgumentException("Distance cannot be negative.");
         }
 
         this.name = name;
         this.speed = speed;
         this.distance = distance;
+        log.debug("Создание Horse, имя [{}], скорость [{}]", name, speed);
     }
 
     public Horse(String name, double speed) {
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+
+import org.slf4j.*;
+
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) throws Exception {
         List<Horse> horses = List.of(
@@ -14,6 +18,7 @@ public class Main {
                 new Horse("Cherry", 3)
         );
         Hippodrome hippodrome = new Hippodrome(horses);
+        log.info("Начало скачек. Количество участников: {}", hippodrome.getHorses().size());
 
         for (int i = 0; i < 100; i++) {
             hippodrome.move();
@@ -23,6 +28,7 @@ public class Main {
 
         String winnerName = hippodrome.getWinner().getName();
         System.out.println(winnerName + " wins!");
+        log.info("Окончание скачек. Победитель: {}", hippodrome.getWinner().getName());
     }
 
     private static void watch(Hippodrome hippodrome) throws Exception {
